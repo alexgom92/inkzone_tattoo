d2ec1baad6f095f4ad5e7bf935b08ef83a867183
alexgom92 <145151769+alexgom92@users.noreply.github.com>
2025-06-15 13:03:38 +0200

    Add files via upload

--- web_tattoo.js
+++ web_tattoo.js
@@ -45,3 +45,12 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 });
 
+document.addEventListener('DOMContentLoaded', () => {
+  const toggle = document.getElementById('menu-toggle');
+  const navList = document.getElementById('nav-list');
+
+  toggle.addEventListener('click', () => {
+    navList.classList.toggle('activo');
+  });
+});
+
